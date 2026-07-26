f4c3fc2a6a4c262924361b68fbd845555f8e8681
santanu Mandal <mandalsantanu331@gmail.com>
2026-07-26 15:01:13 +0530

    add logout features

--- controllers/authController.js
+++ controllers/authController.js
@@ -73,3 +73,22 @@ export const login = async (req, res) => {
         });
     }
 }
+// Logout User
+export const logout = async (req, res) => {
+  try {
+    res.cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0), // Expire immediately
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
